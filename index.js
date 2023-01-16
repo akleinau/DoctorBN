2e0f12490998f854298b48def0e36e171bfc3eb0
akleinau <kleinau@ovgu.de>
2023-01-16 16:02:37 +0100

    bugfix

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ let fs = require('fs');
 fs.readFile('index.html', 'utf8', function(err, data) {
 
     const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.writeHead(200, {'Content-Type': 'text/html'});     
-        res.write(data)
-        res.end();
+        const url = req.url;
+
+        if (url === "/rubin.herokuapp.com") {
+            // do a 302 redirect
+            res.writeHead(302, {
+              location: "https://rubin.herokuapp.com",
+            });
+            res.end();
+          }
+        else {
+            // do a 200 response
+            res.writeHead(200, {'Content-Type': 'text/html'});     
+            res.write(data)
+            res.end();
+        }
+        
+
     }).listen(process.env.PORT || 3000);
 
 });
