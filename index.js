a4abafd5a2c310d281502a25f91cb8249fdad580
akleinau <kleinau@ovgu.de>
2023-01-16 15:54:58 +0100

    bugfix

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const http = require('node:http');
 let fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
 fs.readFile('index.html', 'utf8', function(err, data) {
 
     const server = http.createServer((req, res) => {
@@ -11,10 +8,6 @@ fs.readFile('index.html', 'utf8', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});     
         res.write(data)
         res.end();
-    });
-
-    server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-    });
+    }).listen(process.env.PORT || 3000);
 
 });
